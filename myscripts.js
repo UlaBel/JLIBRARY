84b2d1d6a2fd55cf834c94e53b48e63a06b3137d
UlaBel <115467447+UlaBel@users.noreply.github.com>
2022-12-11 00:34:26 +0100

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -4,3 +4,9 @@ document.querySelector("#show-signup").addEventListener("click",function(){
  document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
  }); 
+ document.querySelector("#show-signup2").addEventListener("click",function(){
+   document.querySelector(".popup2").classList.add("active");
+}); 
+document.querySelector(".popup2 .close-btn").addEventListener("click",function(){
+   document.querySelector(".popup2").classList.remove("active");
+}); 
